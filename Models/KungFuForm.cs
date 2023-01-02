4d3b16423312e9a37a4e5dbc2b50c0e2bda45cd3
Michael Kreich <jaykay2k@hotmail.com>
2023-01-02 21:46:44 +0100

    added property containing training dates

--- Models/KungFuForm.cs
+++ Models/KungFuForm.cs
@@ -2,11 +2,20 @@
 
 public class KungFuForm
 {
-    public static KungFuForm Empty => new KungFuForm(String.Empty);
+    public static KungFuForm Empty => new(string.Empty);
     public string Name { get; }
+
+    public DateTimeOffset[] TrainedDates { get; }
+
+    public KungFuForm(string name, DateTimeOffset[] trainedDates)
+    {
+        Name = name;
+        TrainedDates = trainedDates;
+    }
 
     public KungFuForm(string name)
     {
         Name = name;
+        TrainedDates = Array.Empty<DateTimeOffset>();
     }
 }
