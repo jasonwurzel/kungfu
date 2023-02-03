2588a8f986f6306edabdfa52ebc8b43f66bc1844
Michael Kreich <jaykay2k@hotmail.com>
2023-02-03 10:16:54 +0100

    clever algo for getting the next form

--- Services/KungfuRandomizer.cs
+++ Services/KungfuRandomizer.cs
@@ -7,7 +7,7 @@ public class KungFuRandomizer : IKungfuRandomizer
 {
     private readonly Random _random;
     private readonly IReadOnlyCollection<KungFuForm> _kungFuForms;
-    private readonly List<int> _usedIndices = new();
+    private readonly List<string> _usedForms = new();
 
     public KungFuRandomizer(IReadOnlyCollection<KungFuForm> kungFuForms)
     {
@@ -18,17 +18,54 @@ public class KungFuRandomizer : IKungfuRandomizer
     {
         // of course, this algo can be Leetcode-style optimized. But we won't have more than some dozen forms, so it doesn't matter.
         int randomIndex = -1;
-        if (_usedIndices.Count == _kungFuForms.Count) 
-            _usedIndices.Clear();
+
+        var leastTrainedKungFuForms = takeLeastTrainedKungFuForms();
+        var oldestNotTrainedKungFuForms = takeOldestNotTrainedKungFuForm(leastTrainedKungFuForms);
+
+        if (!oldestNotTrainedKungFuForms.Any())
+            return _kungFuForms.First();
+
+        // if we used all from the current collection as a possible form, start again
+        if (oldestNotTrainedKungFuForms.All(f => _usedForms.Contains(f.Name))) 
+            _usedForms.Clear();
+
+        // if there is any form in usedForms that is NOT in our current collection, start again. This would mean our current collection has changed.
+        if (_usedForms.Any(formName => !oldestNotTrainedKungFuForms.Select(f => f.Name).Contains(formName))) 
+            _usedForms.Clear();
+
+        KungFuForm randomForm;
         do
         {
-            randomIndex = _random.Next(0, _kungFuForms.Count);
-        } while (_usedIndices.Any(used => used == randomIndex));
+            randomIndex = _random.Next(0, oldestNotTrainedKungFuForms.Count);
+            randomForm = oldestNotTrainedKungFuForms.ElementAt(randomIndex);
+        } while (_usedForms.Any(used => used == randomForm.Name));
 
-        _usedIndices.Add(randomIndex);
+        _usedForms.Add(randomForm.Name);
 
-        var randomForm = _kungFuForms.ElementAt(randomIndex);
 
         return randomForm;
+    }
+
+    private IReadOnlyCollection<KungFuForm> takeOldestNotTrainedKungFuForm(IReadOnlyCollection<KungFuForm> leastTrainedKungFuForms)
+    {
+        if (!leastTrainedKungFuForms.Any())
+            return _kungFuForms;
+
+        return leastTrainedKungFuForms
+            .GroupBy(f =>
+            {
+                // MaxBy throws if empty...
+                if (!f.TrainedDates.Any())
+                    return DateTimeOffset.MinValue;
+
+                return f.TrainedDates.MaxBy(dt => dt);
+            })
+            .MinBy(forms => forms.Key)
+            ?.ToList() ?? _kungFuForms;
+    }
+
+    private IReadOnlyCollection<KungFuForm> takeLeastTrainedKungFuForms()
+    {
+        return _kungFuForms.GroupBy(f => f.TrainedDates.Count).MinBy(f => f.Key)?.ToList() ?? _kungFuForms;
     }
 }
--- UnitTests/KungfuRandomizerTests.cs
+++ UnitTests/KungfuRandomizerTests.cs
@@ -50,6 +50,32 @@ public class KungfuRandomizerTests
         originalForms.Should().OnlyContain(form => randomForms.Count(f => f == form) == 2);
     }
 
+    [Fact]
+    public void Calling_NextRandomForm_Respects_Times_Trained()
+    {
+        // Arrange
+        var dates = new List<DateTimeOffset>();
+        for (int i = 1; i < 8; i++)
+        {
+            var dt = new DateTimeOffset(2012, 01, i, 12, 0, 0, TimeSpan.Zero);
+            dates.Add(dt);
+        }
+        var originalForms = new KungFuForm[]
+        {
+            new("Sap Saam Cheung", new []{dates[0], dates[5]}),
+            new("Tang Lung Cheung", new []{dates[1]}),
+            new("Tang Lung Cheung2", new []{dates[1]}),
+            new("Siu Lum Guan", new []{dates[2]}),
+            new("Ng Lun Ma", new []{dates[3]}),
+            new("Fu Mei Daan Dou", new[] { dates[4] })
+        };
+        var sut = new KungFuRandomizer(originalForms);
 
+        // Act
+        var uniqueRandomForms = Enumerable.Range(0, 5).Select(_ => sut.NextRandomForm()).ToArray();
 
+        // Assert
+        uniqueRandomForms.Should().OnlyContain(f => f == originalForms[1] || f == originalForms[2], "because only form 1 and 2 have only 1 training date and are the oldest.");
+        //uniqueRandomForms.Should().BeEquivalentTo(originalForms.Skip(1).Take(2), "because only form 1 and 2 have only 1 training date and are the oldest.");
+    }
 }
